8801642e9fa15b93516e5cb9687145ffd2216eef
David Mamaliga <davidmamaliga@Davids-MacBook-Air.local>
2026-03-03 13:19:29 +0000

    set up git

--- simple_test_project01/simple_test_root/pages/views.py
+++ simple_test_project01/simple_test_root/pages/views.py
@@ -4,4 +4,6 @@ from .models import Page
 
 def index(request):
     pg = Page.objects.get(permalink='/')
-    return HttpResponse(pg.bodytext)
+    title = pg.title
+    bodytxt = pg.bodytext
+    return render(request, 'index.html')
